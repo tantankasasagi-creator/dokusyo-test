34e58629e84569e345365ba976ee5bf39a5e2878
tantankasasagi-creator <tantankasasagi@gmail.com>
2026-06-18 16:10:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2069,6 +2069,8 @@ formData.append('image', resizedFile);
     });
 
     const data = await response.json();
+    console.log(data);
+alert(JSON.stringify(data));
     
     if (!data.success) {
       throw new Error(data.message || 'OCRに失敗しました');
